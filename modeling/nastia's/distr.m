29fea38afcd98e1d28e9148b51df35a53373b1a8
Anastasia Lyulina <30357046+alyulina@users.noreply.github.com>
2021-08-21 19:42:26 -0700

    Delete modeling/nastia's directory

--- modeling/nastia's/distr.m
+++
@@ -1,7 +0,0 @@
-Mathematica 11.1.0
-Off[General::stop]
-
-distr[\[Mu]_,\[Sigma]_,\[Alpha]_]:=NIntegrate[PDF[TruncatedDistribution[{\[Mu]-5*\[Sigma],\[Mu]+5*\[Sigma]},SkewNormalDistribution[\[Mu],\[Sigma],\[Alpha]]],s],{s,\[Mu]-5*\[Sigma],0}]
-d0=Table[distr[Chop[\[Mu]],\[Sigma],\[Alpha]],{\[Alpha],-10,1,0.56},{\[Sigma],10^-8,2.5*10^-4,1.3*10^-5},{\[Mu],-1*10^-4,5*10^-5,0.76*10^-5}];
-
-Export["distr-20.csv",d0]
