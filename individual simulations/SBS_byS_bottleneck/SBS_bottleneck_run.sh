975c818d65da0d21a70fc6047c23440b3d36e2c2
Fyodor Kondrashov <fkondrashov@gmail.com>
2024-11-16 12:01:54 +0900

    Add files via upload

---
+++ individual simulations/SBS_byS_bottleneck/SBS_bottleneck_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=compute
+#SBATCH --job-name=SBS_Multiple_Jobs
+#SBATCH --output=output_%A_%a.out
+#SBATCH --error=error_%A_%a.err
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --array=1-30                # Array range (submit 30 jobs)
+
+
+
+# Execute your program, using SLURM_ARRAY_TASK_ID to differentiate between jobs
+/flash/KondrashovU/FedyaK/SBS_byS_bottleneck/target/release/SBS_byS_bottleneck --output output_${SLURM_ARRAY_TASK_ID}.txt
